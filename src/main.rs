c244b6487f904908ae56a1a114508ff363e86839
Nick Driscoll <nick.a.mavros@gmail.com>
2018-08-14 15:32:21 -0400

    Added vector of entities

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ extern crate sdl2;
 mod vector2;
 mod player;
 mod entity;
-mod command;
 mod keyboard_manager;
 
-use sdl2::event::Event;
 use sdl2::pixels::Color;
 use sdl2::keyboard::Keycode;
 use entity::Entity;
+use std::vec::Vec;
+use std::process; //Remove this when actual quit command is implemented
 
 fn main() {
 	let sdl_context = sdl2::init().unwrap();
@@ -20,13 +20,22 @@ fn main() {
 		.unwrap();
 
 	let mut canvas = window.into_canvas().build().unwrap();
-	let mut event_pump = sdl_context.event_pump().unwrap();
+	let event_pump = sdl_context.event_pump().unwrap();
 
 	//Initialize keyboard manager
 	let mut keyboard_manager = keyboard_manager::new(event_pump);
 
-	let mut player: player::Player = player::new(vector2::new(100, 100));
+	//Add mapping to close the game when you presss Escape
+	keyboard_manager.add_binding(Keycode::Escape, || { process::exit(0); });
 
+	//Create player
+	let mut player = player::new(vector2::new(100, 200));
+
+	//Initialize vector of entities
+	let mut entities: Vec<&mut Entity> = Vec::new();
+
+	//Add player to entities
+	entities.push(&mut player);
 	'running: loop {
 		//Handle input
 		keyboard_manager.handle_keyboard();
@@ -34,8 +43,15 @@ fn main() {
 		canvas.set_draw_color(Color::RGB(0, 255, 255));
 		canvas.clear();
 		
-		player.update();
-		player.draw(&mut canvas);
+		//Update entities
+		for entity in entities.iter_mut() {
+			entity.update();
+		}
+
+		//Draw entities
+		for entity in entities.iter() {
+			entity.draw(&mut canvas);
+		}
 
 		canvas.present();
 	}
