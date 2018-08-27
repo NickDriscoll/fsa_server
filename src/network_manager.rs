2f93f4b340e4235c7e6c71f0ae37bf234ce0344a
Nick Driscoll <nick.a.mavros@gmail.com>
2018-08-27 19:57:42 -0400

    Idk wtf is happening

--- src/network_manager.rs
+++ src/network_manager.rs
@@ -12,22 +12,17 @@ pub fn begin_listening() -> NetworkManager {
 	let listener_thread = thread::spawn(|| {
 		let listener = match TcpListener::bind("127.0.0.1:1337") {
 			Ok(r) => {
+				println!("Bound to port 1337.");
 				r
 			}
 			Err(e) => {
 				panic!("Unable to bind TcpListener.");
 			}
 		};
+		println!("Listener is {:?}", listener);
 
 		for stream in listener.incoming() {
-			match stream {
-				Ok(result) => {
-					println!("Client received!");
-				}
-				Err(e) => {
-					println!("There was an error receiving a client.");
-				}
-			}
+			println!("Stream detected.")
 		}
 	});
 
