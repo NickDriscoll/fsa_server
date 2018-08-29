d030627b266967f922d86b6e64074415a4b967f9
Nick Driscoll <nick.a.mavros@gmail.com>
2018-08-29 12:17:19 -0400

    Began implementing handle_input

--- src/network_manager.rs
+++ src/network_manager.rs
@@ -4,14 +4,21 @@ use std::collections::HashMap;
 use std::thread;
 use std::panic;
 use std::vec::Vec;
+use std::sync::mpsc;
+
+const MAX_PLAYERS: usize = 4;
+const BUFFER_SIZE: usize = 8;
 
 pub struct NetworkManager {
 	listener_thread: thread::JoinHandle<()>,
-	phones: Vec<TcpStream>
+	phones: Vec<TcpStream>,
+	rx: mpsc::Receiver<TcpStream>
 }
 
 //Start a thread to listen for incoming client connections
 pub fn begin_listening() -> NetworkManager {
+	let (tx, rx) = mpsc::channel();
+
 	let listener_thread = thread::spawn(move || {
 		let listener = match TcpListener::bind("0.0.0.0:1337") {
 			Ok(r) => {
@@ -26,6 +33,12 @@ pub fn begin_listening() -> NetworkManager {
 
 		for stream in listener.incoming() {
 			println!("Stream detected.");
+			match tx.send(stream.unwrap()) {
+				Err(e) => {
+					println!("An error occurred while sending data over a channel: {:?}", e);
+				}
+				_ => { }
+			}
 		}
 	});
 
@@ -33,12 +46,29 @@ pub fn begin_listening() -> NetworkManager {
 
 	NetworkManager {
 		listener_thread,
-		phones: Vec::new()
+		phones: Vec::with_capacity(MAX_PLAYERS),
+		rx
 	}
 }
 
 impl NetworkManager {
-	pub fn handle_input() {
+	pub fn handle_input(&mut self) {
+		//First thing to do is check if there are any pending connections
+		match self.rx.try_recv() {
+			Ok(stream) => {
+				if (self.phones.len() < MAX_PLAYERS) {
+					self.phones.push(stream);
+				}
+			}
+			Err(e) => {
+				println!("{:?}", e);
+			}
+		}
 
+		//For each stream, emit commands based on what's in the stream buffer
+		let mut buffer: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+		for stream in self.phones.iter() {
+
+		}
 	}
 }
