b558198439c78fdd2dba3e192c5f8b6dcdce7af7
Nick Driscoll <nick.a.mavros@gmail.com>
2018-08-29 10:42:22 -0400

    Added Vec<TcpStream> to NetworkManager

--- src/network_manager.rs
+++ src/network_manager.rs
@@ -1,15 +1,18 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
 use std::collections::HashMap;
 use std::thread;
 use std::panic;
+use std::vec::Vec;
 
 pub struct NetworkManager {
-	listener_thread: thread::JoinHandle<()>
+	listener_thread: thread::JoinHandle<()>,
+	phones: Vec<TcpStream>
 }
 
 //Start a thread to listen for incoming client connections
 pub fn begin_listening() -> NetworkManager {
-	let listener_thread = thread::spawn(|| {
+	let listener_thread = thread::spawn(move || {
 		let listener = match TcpListener::bind("0.0.0.0:1337") {
 			Ok(r) => {
 				println!("Bound to port 1337.");
@@ -22,17 +25,20 @@ pub fn begin_listening() -> NetworkManager {
 		println!("Listener is {:?}", listener);
 
 		for stream in listener.incoming() {
-			println!("Stream detected.")
+			println!("Stream detected.");
 		}
 	});
 
 	println!("Started listening...");
 
 	NetworkManager {
-		listener_thread
+		listener_thread,
+		phones: Vec::new()
 	}
 }
 
 impl NetworkManager {
-	
+	pub fn handle_input() {
+
+	}
 }
